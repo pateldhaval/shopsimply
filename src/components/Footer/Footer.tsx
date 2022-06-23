7cdfe730d6783dbab4aee082099e0c4508835646
Dhaval Patel <pateldhaval3890@gmail.com>
2022-06-23 09:59:17 +0530

    Header & Footer refinements

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,8 +6,10 @@ interface Props {
 
 export const Footer: React.FC<Props> = (props) => {
 	return (
-		<footer className='p-4 border-t'>
-			<div className='container'>Footer</div>
+		<footer className='py-3 border-t'>
+			<div className='container'>
+				<span className='font-bold'>ShopSimply</span>, All rights reserved.
+			</div>
 		</footer>
 	);
 };
