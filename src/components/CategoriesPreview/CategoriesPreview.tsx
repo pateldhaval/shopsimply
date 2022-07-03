fdcfdb5330c033a930a6409e1eb91a74ddebded7
Dhaval Patel <pateldhaval3890@gmail.com>
2022-07-03 15:42:11 +0530

    Products component rename to CategoryPreview

--- src/components/CategoriesPreview/CategoriesPreview.tsx
+++ src/components/CategoriesPreview/CategoriesPreview.tsx
@@ -1,30 +1,33 @@
-// import './CategoriesPreview.css';
+// import './Categories.css';
 
-import { ProductCard } from '../ProductCard';
-import { SectionTitle } from '../SectionTitle';
+import React from 'react';
 
-interface Props {
-	title: string;
-	products: any[];
-}
+import { CategoryPreviewItem } from '@/components/CategoryPreviewItem';
+import { Section } from '@/components/Section';
+import { useShopContext } from '@/utils/context/Shop.context';
+
+interface Props {}
 
 export const CategoriesPreview: React.FC<Props> = (props) => {
-	const { title, products } = props;
+	const { categoriesMap } = useShopContext();
 
 	return (
-		<div>
-			<SectionTitle>{title.toUpperCase()}</SectionTitle>
-			<div className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-10'>
-				{products
-					// Filter first 4 products from the array
-					.filter((_, index) => index < 4)
-					// Then show remaining products
-					.map((product) => (
-						<div className='col-span-1' key={product.id}>
-							<ProductCard product={product} />
-						</div>
-					))}
+		<Section>
+			<div className='space-y-20'>
+				{Object.keys(categoriesMap).map((title) => {
+					const category = categoriesMap[title];
+					return (
+						category &&
+						Object.keys(category).length > 0 && (
+							<CategoryPreviewItem
+								key={title}
+								title={title}
+								products={category.products}
+							/>
+						)
+					);
+				})}
 			</div>
-		</div>
+		</Section>
 	);
 };
--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -1,9 +1,9 @@
-import { Products } from '@/components/Products';
+import { CategoriesPreview } from '@/components/CategoriesPreview';
 
 const Shop = () => {
 	return (
 		<>
-			<Products />
+			<CategoriesPreview />
 		</>
 	);
 };
