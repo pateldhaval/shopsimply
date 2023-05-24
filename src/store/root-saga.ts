a811299ecc22daf4318f25ad03b6b0b4e47bfab0
Dhaval Patel <pateldhaval3890@gmail.com>
2023-05-24 09:34:34 +0530

    categories saga removed

--- src/store/root-saga.ts
+++ src/store/root-saga.ts
@@ -1,14 +1,12 @@
 import { all, call } from 'redux-saga/effects';
 
 import { authSaga } from './auth/auth.saga';
-import { categoriesSaga } from './categories/categories.saga';
 import { signupSaga } from './signup/signup.saga';
 
 export function* rootSaga() {
 	yield all([
 		// All features side-effects combined and init from root
 		call(authSaga),
-		call(signupSaga),
-		call(categoriesSaga)
+		call(signupSaga)
 	]);
 }
